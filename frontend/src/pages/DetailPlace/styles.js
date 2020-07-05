20c412f448585e6518257cdaaf66236a31c504c7
Joacy <joacymsilva@gmail.com>
2020-07-05 15:30:44 -0300

    Ajustes na imagem do estabelecimento

--- frontend/src/pages/DetailPlace/styles.js
+++ frontend/src/pages/DetailPlace/styles.js
@@ -17,8 +17,6 @@ export const BoxDetail = styled.div`
 
     img {
         width: 100%;
-        height: auto;
-        object-fit: contain;
         margin-bottom: 15px;
         border-radius: 4px;
     }
